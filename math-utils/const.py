8fa29e0b743dfb4c369d19e39bc8c8785f99437c
tc0512 <tancheng_0812@qq.com>
2026-03-01 01:18:31 +0000

    常数模块完成

--- math-utils/const.py
+++ math-utils/const.py
@@ -1,6 +1,20 @@
-def pi():
+def pi(): #π
     return 3.1415926535897932
-def e():
+def e(): #e
     return 2.7182818284590452
-def tau():
+def tau(): #2π
     return 6.2831853071795864
+def phi(): #φ
+    return 1.6180339887498948
+def G(): #万有引力常数(N·m^2/kg^2)
+    return 6.67430e-11
+def c(): #光速(m/s)
+    return 299792458
+def h(): #普朗克常数(J·s)
+    return 6.62607015e-34
+def g(): #重力加速度(N/kg)
+    return 9.80665
+def k(): #玻尔兹曼常数(J/K)
+    return 1.380649e-23
+def NA(): #阿伏伽得罗常数(mol^-1)
+    return 6.02215076e23
